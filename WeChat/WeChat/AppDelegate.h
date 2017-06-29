1c816ba1590b994023dd44ea432e467dd312d67c
Zhong <amtfzhongyunhe@163.com>
2017-06-29 13:16:55 +0800

    初始化一下项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by 云鹤 on 2017/6/29.
+//  Copyright © 2017年 云鹤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by 云鹤 on 2017/6/29.
+//  Copyright © 2017年 云鹤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
